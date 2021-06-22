ad3a8ae079162d79031cebe70f6e8ea5aa02260e
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2021-06-22 12:17:05 -0300

    remove embed area

--- template-parts/releases-content.php
+++ template-parts/releases-content.php
@@ -4,12 +4,9 @@
 					<li class="<?php the_field('item')?>-item release" data-info=".<?php the_field('item')?>">
             
             <div class="release-cover" data-embed=".<?php the_field('item')?>-embed"> 
-             <a class="link_to_bandcamp"  target="_blank"><?php the_post_thumbnail() ?></a>
-
+             <a class="link_to_bandcamp" href="<?php the_field('href')?>" target="_blank"><?php the_post_thumbnail() ?></a>
             </div>
        
-
-
           </li>
 
      
